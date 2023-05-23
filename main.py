c5ea0642a63084c9ead0bfa9fe0ead7ba3bc890c
Fabian Fasth <fabian.fasth@gmail.com>
2023-05-23 16:18:44 +0200

    config file and udp controller

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-COLUMN_NAME_FOR_DATETIME = "Fecha envio"
-COLUMNS_TO_SIMUMATIK = ['SP_Aluminio', 'PV_Aluminio', 'Kilos total lingotes', 'Total escorias']
-
 import pandas as pd
 import time
+import json
+from Controller import UDP_Controller
 
+# Read config file
+f = open('config.json')
+config = json.load(f)
+print(config)
+    
 def read_data(filename='data.csv', debug:bool=False):
+    global config
+    print(config)
     start_time = time.time()
     data = pd.read_csv(filename, low_memory=False) #  index_col=0,
 
@@ -18,25 +24,75 @@ def read_data(filename='data.csv', debug:bool=False):
 
     start_time = time.time()
     # Convert time to datetime
-    data[COLUMN_NAME_FOR_DATETIME] = pd.to_datetime(data[COLUMN_NAME_FOR_DATETIME], format='mixed')
+    data[config["COLUMN_NAME_FOR_DATETIME"]] = pd.to_datetime(data[config["COLUMN_NAME_FOR_DATETIME"]], format='mixed')
     if debug:
         print("transform data %s seconds ---" % (time.time() - start_time))
         print(data)
+    
+    types = data.dtypes
+    if debug:
+        print(types["PV_Aluminio"])
         
-    return data
+    return data, types
 
+def connect_controller(initial_values, types):
+    global config
+    
+    _controller = UDP_Controller()
+    
+    for variable in config["COLUMNS_TO_SIMUMATIK"]:
+        if types[variable] == "float64":
+            data_type = "float"
+        elif types[variable] == "int64":
+            data_type = "int"
+        else:
+            data_type = "str"
+            
+        _controller.addVariable(variable, data_type, initial_values[variable])
+    
+    for variable in config["Variables_from_simumatik"]:
+        pass
+    
+    _controller.start()
+    
+    return _controller
 
-
-if __name__ == "__main__":
-    data = read_data("data.csv", debug=True)
+if __name__ == "__main__":    
+    data, types = read_data("data.csv", debug=False)
+    
+    #controller = connect_controller(data.loc[0], types)
+    _controller = UDP_Controller()
+    
+    for variable in config["COLUMNS_TO_SIMUMATIK"]:
+        if types[variable] == "float64":
+            data_type = "float"
+        elif types[variable] == "int64":
+            data_type = "int"
+        else:
+            data_type = "str"
+            
+        _controller.addVariable(variable, data_type, 0)
+    
+    for variable in config["Variables_from_simumatik"]:
+        pass
+    
+    _controller.start()
+    
     
     for i in range(len(data)):
-        print(f"Time : {data.loc[i][COLUMN_NAME_FOR_DATETIME]}")
+        print(f"Time : {i}", end="\r")
+        #print(f"Time : {data.loc[i][config['COLUMN_NAME_FOR_DATETIME']]}", end="\r")
+        # print(f"Time diff : {data.loc[i][config['COLUMN_NAME_FOR_DATETIME']]-data.loc[0][config['COLUMN_NAME_FOR_DATETIME']]}")
         
-        for col in COLUMNS_TO_SIMUMATIK:
-            print(f"{col} : {data.loc[i][col]}")
+        # for col in config["COLUMNS_TO_SIMUMATIK"]:
+        #     print(f"{col} : {type(data.loc[i][col])}")
         
-        print()
+        # print()
+        for col in config["COLUMNS_TO_SIMUMATIK"]:
+            _controller.setValue(col, data.loc[i][col])
         
-        if i > 10:
-            break
+        time.sleep(1e-5)
+        
+        # if i > 10000:
+            
+        #     break
