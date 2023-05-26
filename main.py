21ee7de673c4ff457c941bef0248a88b92f5125c
Fabian Fasth <fabian.fasth@gmail.com>
2023-05-26 15:59:35 +0200

    Communication with simumatik

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from Controller import UDP_Controller
 # Read config file
 f = open('config.json')
 config = json.load(f)
-print(config)
+# print(config)
     
 def read_data(filename='data.csv', debug:bool=False):
     global config
-    print(config)
+
     start_time = time.time()
     data = pd.read_csv(filename, low_memory=False) #  index_col=0,
 
@@ -22,13 +22,21 @@ def read_data(filename='data.csv', debug:bool=False):
     if debug:
         print("dropNA %s seconds ---" % (time.time() - start_time))
 
-    start_time = time.time()
     # Convert time to datetime
-    data[config["COLUMN_NAME_FOR_DATETIME"]] = pd.to_datetime(data[config["COLUMN_NAME_FOR_DATETIME"]], format='mixed')
+    start_time = time.time()
+    data[config["COLUMN_NAME_FOR_DATETIME"]] = pd.to_datetime(data[config["COLUMN_NAME_FOR_DATETIME"]], dayfirst=True)
     if debug:
         print("transform data %s seconds ---" % (time.time() - start_time))
         print(data)
-    
+
+    # Include timedelta for all lines
+    start_time = time.time()
+    data['time_delta'] = data["Fecha envio"] - data["Fecha envio"].shift()
+    if debug:
+        print("transform data %s seconds ---" % (time.time() - start_time))
+        print(data)
+
+    # Determine data types
     types = data.dtypes
     if debug:
         print(types["PV_Aluminio"])
@@ -36,31 +44,6 @@ def read_data(filename='data.csv', debug:bool=False):
     return data, types
 
 def connect_controller(initial_values, types):
-    global config
-    
-    _controller = UDP_Controller()
-    
-    for variable in config["COLUMNS_TO_SIMUMATIK"]:
-        if types[variable] == "float64":
-            data_type = "float"
-        elif types[variable] == "int64":
-            data_type = "int"
-        else:
-            data_type = "str"
-            
-        _controller.addVariable(variable, data_type, initial_values[variable])
-    
-    for variable in config["Variables_from_simumatik"]:
-        pass
-    
-    _controller.start()
-    
-    return _controller
-
-if __name__ == "__main__":    
-    data, types = read_data("data.csv", debug=False)
-    
-    #controller = connect_controller(data.loc[0], types)
     _controller = UDP_Controller()
     
     for variable in config["COLUMNS_TO_SIMUMATIK"]:
@@ -74,25 +57,34 @@ if __name__ == "__main__":
         _controller.addVariable(variable, data_type, 0)
     
     for variable in config["Variables_from_simumatik"]:
-        pass
+        _controller.addVariable(variable, "float", 0)
     
     _controller.start()
+    return _controller
+
+if __name__ == "__main__":    
+    data, types = read_data("data.csv", debug=True)
+
+    _controller = connect_controller(data.loc[0], types)
     
-    
-    for i in range(len(data)):
-        print(f"Time : {i}", end="\r")
-        #print(f"Time : {data.loc[i][config['COLUMN_NAME_FOR_DATETIME']]}", end="\r")
-        # print(f"Time diff : {data.loc[i][config['COLUMN_NAME_FOR_DATETIME']]-data.loc[0][config['COLUMN_NAME_FOR_DATETIME']]}")
-        
-        # for col in config["COLUMNS_TO_SIMUMATIK"]:
-        #     print(f"{col} : {type(data.loc[i][col])}")
-        
-        # print()
+    while _controller.getValue("emulation_time") == 0.0:
+        print("Waiting for simulation model to connect...", end="\r")
+        time.sleep(1)
+    print()
+
+    for i in range(len(data)-1):
         for col in config["COLUMNS_TO_SIMUMATIK"]:
             _controller.setValue(col, data.loc[i][col])
         
-        time.sleep(1e-5)
-        
-        # if i > 10000:
-            
-        #     break
+        # Read values from Simumatik:
+        time_relation = _controller.getValue("time_relation")
+        emulation_time = _controller.getValue('emulation_time')
+
+        # calculate time to sleep
+        if time_relation > 0:
+            delta = data.loc[i+1]['time_delta'] / time_relation
+            delta = delta.seconds + delta.microseconds/1000000
+            print(f"Time : {emulation_time}, line: {i}, delta: {delta}                 ", end="\r")  
+            time.sleep(delta)
+
+    print("\nAll lines read.")
