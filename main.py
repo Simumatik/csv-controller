a9a5c60048a3e412129a37c9a2694933f266fd0b
Fabian Fasth <fabian.fasth@gmail.com>
2023-06-05 13:56:16 +0200

    stop for emulation pause

--- main.py
+++ main.py
@@ -15,31 +15,43 @@ def read_data(filename='data.csv', debug:bool=False):
     start_time = time.time()
     data = pd.read_csv(filename, low_memory=False) #  index_col=0,
     if debug:
-        print("reading %s seconds ---" % (time.time() - start_time))
+        print("--- reading data %s seconds ---" % (time.time() - start_time))
 
     # Removing columns with NaN
     start_time = time.time()
     data.dropna(axis=1, inplace=True) 
     if debug:
-        print("dropNA %s seconds ---" % (time.time() - start_time))
+        print("--- dropNA %s seconds ---" % (time.time() - start_time))
+
+    # Drop lines that ain't Maguina A
+    start_time = time.time()
+    data.drop(data.query(" `Maquina`!='A' ").index, inplace=True)
+    if debug:
+        print("--- dropNA %s seconds ---" % (time.time() - start_time))
 
     # Drop columns that is not used
     start_time = time.time()
     data = data.filter(config["COLUMNS_TO_SIMUMATIK"])
     if debug:
-        print("dropColumns %s seconds ---" % (time.time() - start_time))
+        print("--- Drop lines that ain't Maguina A %s seconds ---" % (time.time() - start_time))
 
     # Convert time to datetime
     start_time = time.time()
     data[config["COLUMN_NAME_FOR_DATETIME"]] = pd.to_datetime(data[config["COLUMN_NAME_FOR_DATETIME"]], dayfirst=True)
     if debug:
-        print("transform data %s seconds ---" % (time.time() - start_time))
+        print("--- Convert time to datetime %s seconds ---" % (time.time() - start_time))
+
+    # Sort on Date time
+    start_time = time.time()
+    data.sort_values(by=config["COLUMN_NAME_FOR_DATETIME"], inplace=True, ignore_index=True)
+    if debug:
+        print("--- sort by date  %s seconds ---" % (time.time() - start_time))
 
     # Include timedelta for all lines
     start_time = time.time()
     data['time_delta'] = data["Fecha envio"] - data["Fecha envio"].shift()
     if debug:
-        print("transform data %s seconds ---" % (time.time() - start_time))
+        print("--- calculate time_delta %s seconds ---" % (time.time() - start_time))
 
     # Determine data types
     types = data.dtypes
@@ -73,24 +85,39 @@ if __name__ == "__main__":
 
     _controller = connect_controller(data.loc[0], types)
     
-    while _controller.getValue("emulation_time") == 0.0:
+    while _controller.getValue('emulation_time') == 0.0:
         print("Waiting for simulation model to connect...", end="\r")
-        time.sleep(1)
+        time.sleep(0.1)
+
     print()
 
+    last_pause_check = 0
     for i in range(len(data)-1):
         for col in config["COLUMNS_TO_SIMUMATIK"]:
             _controller.setValue(col, data.loc[i][col])
         
         # Read values from Simumatik:
         time_relation = _controller.getValue("time_relation")
-        emulation_time = _controller.getValue('emulation_time')
+        if i == 0:
+            emulation_time = _controller.getValue('emulation_time')
+        new_time = _controller.getValue('emulation_time')
 
         # calculate time to sleep
         if time_relation > 0:
             delta = data.loc[i+1]['time_delta'] / time_relation
             delta = delta.seconds + delta.microseconds/1000000
             print(f"Time : {emulation_time}, line: {i}, delta: {delta}                 ", end="\r")  
+
+            # Check to see if simulation model paused.
+            if i > last_pause_check + 2*time_relation* delta:
+                last_pause_check = i
+
+                while new_time == emulation_time:
+                    print("---- Simulation model paused ----                     ", end="\r")
+                    time.sleep(0.1)
+                    new_time = _controller.getValue('emulation_time')
+            emulation_time = new_time
+
             time.sleep(delta)
 
     print("\nAll lines read.")
