22f9b063050a06bc2096fd89c5d17eddf6ebc499
Fabian Fasth <fabian.fasth@gmail.com>
2023-06-15 16:27:40 +0200

    fix starting line

--- main.py
+++ main.py
@@ -95,11 +95,11 @@ if __name__ == "__main__":
         last_pause_check = time.time()
 
         try:
-            starting_line = _controller.getValue('starting_line')
+            starting_line = int(_controller.getValue('starting_line'))
         except:
             starting_line = 0
 
-        for i in range(len(data)-1):
+        for i in range(starting_line, len(data)-1):
             # Send values to Simumatik
             for col in config["COLUMNS_TO_SIMUMATIK"]:
                 _controller.setValue(col, data.loc[i][col]) 
